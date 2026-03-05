a70fdec1c3e916ad6bba64b3b917eeec1db1e7b2
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-03-05 13:50:23 +0100

    added  new user data with POST
    -name, age , email n validate with ZOD

--- server/server.ts
+++ server/server.ts
@@ -66,3 +66,47 @@ app.get('/random-person', async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch random person data' });
     }
 });
+
+
+
+// Phase 3 — User POST route 
+// Create POST /users route 
+// Accept an object with name, age, and email 
+// Validate using Zod with rules: 
+//   - name: 3-12 characters 
+//   - age: optional, 18-100, default 28 
+//   - email: must be valid and lowercase 
+// Return 201 with the validated user or 400 with error details 
+
+app.post('/users', (req, res) => {
+    const userSchema = z.object({
+        name: z.string().min(3).max(12),
+        age: z.number().min(18).max(100).optional().default(28),
+        email: z.email().toLowerCase(),      
+    });
+
+    //validate the data using Zod schema-
+    const validateNewUser = userSchema.safeParse(req.body);
+    if (!validateNewUser.success) {
+        console.error('Validation errors:', validateNewUser.error);
+        return res.status(400).json(
+            { error: 'Invalid user data', 
+            details: validateNewUser.error });
+    } else {
+        const newUser = validateNewUser.data;
+        res.status(201).json({ message: 'User created successfully', user: newUser }        
+
+        );
+    }
+});
+
+
+
+// Challenge — Fetch additional data 
+// Create GET /random-login route 
+// Fetch a random user from the RandomUser API 
+// Return their username and registered date 
+// Validate the response with Zod to ensure the fields exist 
+// Optional: Display in the response a short summary like: 'username (registered on YYYY-MM-DD)' 
+
+ 
